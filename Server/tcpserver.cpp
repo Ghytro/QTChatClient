c3f392b4669be8b978d52774618b449d4ffe772a
Michael Korobkov <korobkov200364@gmail.com>
2021-01-15 00:34:59 +0300

    Update tcpserver.cpp

--- Server/tcpserver.cpp
+++ Server/tcpserver.cpp
@@ -40,6 +40,243 @@ void Server::slotReadClient()
     clientSocket->write(reply.toUtf8());
 }
 
+QJsonObject Server::generateErrorJson(const apiErrorCode &err)
+{
+    QJsonObject error;
+    error.insert("error_code", err);
+    error.insert("error_desc", Server::apiErrorCodeDesc(err));
+    return error;
+}
+
+QJsonObject Server::callApiMethod(const QString &method, const QJsonObject &params)
+{
+    if (method == "chat.get")
+    {
+        apiErrorCode apiErr = NULL_ERROR;
+
+        if (params["access_token"] == QJsonValue::Undefined)
+            apiErr = NO_ACCESS_TOKEN;
+
+        else if (params["chat_id"] == QJsonValue::Undefined)
+            apiErr = NO_CHAT_ID;
+
+        else if (params["chat_id"].toInt() < 0 ||
+                 params["access_token"].toString().length() != Server::accessTokenLen)
+            apiErr = INCORRECT_VALUE;
+
+        size_t senderID;
+        try
+        {
+            senderID = Server::getIDFromAccessToken(params["access_token"].toString());
+        }
+        catch (const TokenDoesNotExistException &e)
+        {
+            return Server::generateErrorJson(apiErrorCode::TOKEN_VALIDATION_FAILURE);
+        }
+
+        if (apiErr != NULL_ERROR)
+            return Server::generateErrorJson(apiErr);
+
+        size_t chatID = params["chat_id"].toInt();
+
+        QJsonObject response;
+        try
+        {
+            response = Server::getChatInfo(chatID, senderID);
+        }
+        catch (const ChatIsNotVisibleException &e)
+        {
+            return Server::generateErrorJson(apiErrorCode::CHAT_IS_NOT_VISIBLE);
+        }
+
+        return response;
+    }
+
+    else if (method == "access_token.change")
+    {
+        apiErrorCode apiErr = NULL_ERROR;
+        if (params["access_token"] == QJsonValue::Undefined)
+            apiErr = NO_ACCESS_TOKEN;
+
+        else if (params["user_id"] == QJsonValue::Undefined)
+            apiErr = NO_USER_ID;
+
+        else if (params["password"] == QJsonValue::Undefined)
+            apiErr = NO_USER_PASSWORD;
+
+        else if (params["user_id"].toInt() < 0)
+            apiErr = INCORRECT_VALUE;
+
+        else if (!Server::validateUser(params["user_id"].toInt(),
+                                       params["password"].toString()))
+            apiErr = USER_VALIDATION_FAILURE;
+
+        if (apiErr != apiErrorCode::NULL_ERROR)
+            return Server::generateErrorJson(apiErr);
+
+        QString newToken = Server::updAccessToken(params["user_id"].toInt());
+        QJsonObject response;
+        response.insert("new_token", newToken);
+        return response;
+    }
+
+    return Server::generateErrorJson(apiErrorCode::UNKNOWN_ERROR);
+}
+
+QString Server::apiErrorCodeDesc(const apiErrorCode &err)
+{
+    switch (err)
+    {
+    case NULL_ERROR:
+        return "";
+        break;
+
+    case NO_ACCESS_TOKEN:
+        return "No access token provided to API";
+        break;
+
+    case NO_CHAT_ID:
+        return "Query parameter not found: chat_id";
+        break;
+
+    case NO_QUERY_SENDER_ID:
+        return "Unknown query sender id, unable to validate access token";
+        break;
+
+    case INCORRECT_VALUE:
+        return "Some of the fields of query contain prohibited values";
+        break;
+
+    default:
+        return "Unknown error";
+        break;
+    }
+}
+
+bool Server::validateUser(const size_t &userID, const QString &userPassword)
+{
+    QFile in("dbase/userlogindata");
+    if (!in.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        qDebug() << "Unable to open dbase/userlogindata for reading";
+        return false;
+    }
+    while (!in.atEnd())
+    {
+        QString line = in.readLine();
+        QStringList pairOfValues = line.split(' ');
+        size_t id = pairOfValues[0].toInt();
+        QString password = pairOfValues[1];
+        if (id == userID && password == userPassword)
+            return true;
+    }
+    in.close();
+    return false;
+}
+
+size_t Server::getIDFromAccessToken(const QString &accessToken)
+{
+    QDir().mkdir("dbase");
+    QFile in("dbase/access_tokens");
+    if (!in.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        in.close();
+        if (!in.open(QIODevice::WriteOnly))
+        {
+            qDebug() << "Unable to open access_tokens for writing";
+            return false;
+        }
+        in.close();
+        if (!in.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            qDebug() << "Unable to open access_tokens for reading";
+            return false;
+        }
+    }
+    while (!in.atEnd())
+    {
+        QString line = in.readLine();
+        QStringList pairOfValues = line.split(' ');
+
+        QString token = pairOfValues[1];
+        if (accessToken == token)
+            return pairOfValues[0].toInt();
+    }
+    in.close();
+    throw TokenDoesNotExistException();
+}
+
+QString Server::generateAccessToken()
+{
+    const QString alphabet = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm.-0123456789_=/|";
+    QString ans;
+    for (size_t i = 0; i < Server::accessTokenLen; ++i)
+        ans += alphabet[QRandomGenerator::global()->generate() % alphabet.length()];
+    return ans;
+}
+
+QString Server::updAccessToken(const size_t &senderID)
+{
+    QDir().mkdir("dbase");
+    QFile in("dbase/access_tokens");
+    if (!in.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        in.close();
+        if (!in.open(QIODevice::WriteOnly))
+        {
+            qDebug() << "Unable to open access_tokens for writing";
+            return "";
+        }
+        in.close();
+        if (!in.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            qDebug() << "Unable to open access_tokens for reading";
+            return "";
+        }
+    }
+
+    QStringList temp;
+    QString newAccessToken = Server::generateAccessToken();
+    bool isUpdated = false;
+    while (!in.atEnd())
+    {
+        QString line = in.readLine();
+        QStringList pairOfValues = line.split(' ');
+        size_t id = pairOfValues[0].toInt();
+        if (id == senderID)
+        {
+            isUpdated = true;
+            pairOfValues[1] = newAccessToken;
+        }
+        temp.append(pairOfValues[0] + " " + pairOfValues[1]);
+    }
+
+    if (!isUpdated)
+        temp.append(QStringLiteral("%1 %2").arg(senderID).arg(newAccessToken));
+
+    in.close();
+    if (!in.open(QIODevice::WriteOnly | QIODevice::Truncate))
+    {
+        qDebug() << "Unable to open access_tokens for writing";
+        return "";
+    }
+    QTextStream out(&in);
+    for (QString i: temp)
+        out << i << Qt::endl;
+    qDebug() << "User" << senderID << "token updated";
+    return newAccessToken;
+}
+
+QString Server::parseQuery(const QString &query)
+{
+    QJsonObject jsonObj = QJsonDocument::fromJson(query.toUtf8()).object();
+    QString method = jsonObj["method"].toString();
+    QJsonObject jsonParams = jsonObj["params"].toObject();
+
+    QJsonObject response = Server::callApiMethod(method, jsonParams);
+    return QJsonDocument(response).toJson();
+}
+
 void Server::createChat(const QString&             chatName,
                            const QJsonArray&       membersIDs,
                            const size_t&           adminID,
@@ -190,11 +427,12 @@ QJsonArray Server::getLastBlockOfMessages(const size_t &chatID, const size_t &qu
     return jsonArr;
 }
 
-QJsonObject Server::getChatInfo(const size_t &chatID)
+QJsonObject Server::getChatInfo(const size_t &chatID, const size_t &senderID)
 {
     QFile infoFile(QStringLiteral("chats/%1/info.json").arg(chatID));
     QJsonObject jsonObj = QJsonDocument::fromJson(QString(infoFile.readAll()).toUtf8()).object();
-    if (!jsonObj["is_visible"].toBool())
+    QJsonArray members = jsonObj["members"].toArray();
+    if (!jsonObj["is_visible"].toBool() && !members.contains(QJsonValue::fromVariant(senderID)))
         throw ChatIsNotVisibleException();
     return jsonObj;
 }
